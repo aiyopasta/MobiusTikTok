d528b2b41d9af52e5db204e0a629d8f49de74076
Aditya Abhyankar <adityaa.edu@gmail.com>
2023-10-14 15:12:53 -0400

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 
-def C(t):
-    t = 2 * np.pi * np.asarray(t)
-    u = t / (2 * np.pi)
+def C(t_):
+    t_ = 2 * np.pi * np.asarray(t_)
+    u = t_ / (2 * np.pi)
     h = 100 * (0.5 * u ** 2 - 1.5 * u ** 3 + 1.5 * u ** 4 - 0.5 * u ** 5)
     h += 100 * (0.5 * u ** 3 - u ** 4 + 0.5 * u ** 5)
     h1 = 0.32 * h
-    return np.array([0.5 * np.cbrt(np.sin(t)), 0.6 * h1])
+    return np.array([np.cbrt(np.sin(t_)), h1])
 
 
-def compute_arclength_parametrization(C, N=1000):
+def get_parameterization(xscale, yscale, N=1000):
     t_values = np.linspace(0, 1, N + 1)  # t in [0, 1]
     segment_lengths = np.sqrt(np.sum(np.diff(C(t_values), axis=1) ** 2, axis=0))
     S = np.sum(segment_lengths)
@@ -24,13 +24,13 @@ def compute_arclength_parametrization(C, N=1000):
     def get_point(s):
         # Ensure s is in [0, 1]
         s = np.clip(s, 0, 1)
-        return np.array([x_interp(s), y_interp(s)])
+        return np.array([xscale * x_interp(s), yscale * y_interp(s)])
 
-    return get_point, S
+    return get_point
 
 
 # Example usage
-get_point, S = compute_arclength_parametrization(C, N=200)
+get_point = get_parameterization(837.5, 628.125, N=200)
 
 # Plotting
 fig, ax = plt.subplots(figsize=(8, 8))
